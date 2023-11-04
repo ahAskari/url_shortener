a398d17e38ec4409d5afca607afd16639010867a
ahAskari <ah.askari8@gmail.com>
2023-11-04 12:21:55 +0330

    Fix server error

---
+++ shortener_app/config.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from functools import lru_cache
+
+
+class Settings(BaseModel):
+    env_name: str = "Local"
+    base_url: str = "http://localhost:8000"
+    db_url: str = "sqlite:///./shortener.db"
+
+    class Config:
+        env_file = '.env'
+
+
+@lru_cache
+def get_settings() -> Settings:
+    setting = Settings()
+    return setting
